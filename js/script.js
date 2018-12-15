ecf64871ad5c9691fb961d984c6490e2bf637ceb
cwinters8 <clwinters8@gmail.com>
2018-12-14 19:57:45 -0800

    add functions to append page links and manipulate data

--- js/script.js
+++ js/script.js
@@ -16,9 +16,7 @@ FSJS project 2 - List Filter and Pagination
    will only be used inside of a function, then it can be locally 
    scoped to that function.
 ***/
-
-
-
+const students = document.getElementsByClassName('student-item');
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
@@ -34,17 +32,54 @@ FSJS project 2 - List Filter and Pagination
        that will be passed into the parens later when you call or 
        "invoke" the function 
 ***/
+function showPage() {
 
-
+}
 
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
-
-
-
-
+// creates as many page links as the number passed
+function appendPageLinks(num) {
+    // get parent div
+    const parent = document.getElementsByClassName('page')[0];
+    const div = document.createElement('div');
+    div.className = 'pagination';
+    const ul = document.createElement('ul');
+    div.appendChild(ul);
+    parent.appendChild(div);
+    for (let i = 0; i < num; i++) {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.href = '#';
+        a.textContent = (i + 1);
+        li.appendChild(a);
+        ul.appendChild(li);
+    }
+    return(div);
+}
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
+
+
+// build an array of arrays, with each child array having ten or less elements
+function arrayOfArrays(students) {
+    const tens = Math.ceil(students.length / 10);
+    let parentArray = [];
+    let bottom = 0;
+    for (let i = 1; i <= tens; i++) {
+        const top = i * 10;
+        let childArray = [];
+        for (let j = bottom; j < top; j++) {
+            if (j < students.length) {
+                childArray.push(students[j]);
+            }
+        }
+        bottom = bottom + 10;
+        parentArray.push(childArray);
+    }
+    return(parentArray);
+}
+
